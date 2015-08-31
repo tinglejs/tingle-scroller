69c8fd8b0b2c56ef5efa8123058a384d2be33a14
曹柯 <caoke.ck@alibaba-inc.com>
2015-08-31 23:01:53 +0800

    add deps

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,4 @@
 var Context = require('tingle-context');
+window.FaskClick && FastClick.attach(document.body);
 var Demo = require('./ScrollerDemo');
 React.render(<Demo/>, document.getElementById('TingleDemo'));
